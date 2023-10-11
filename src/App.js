b805a542e18d9641a2bc29b8726fe395fdd90f3c
telegramstore <pavel-romanov-91@mail.ru>
2023-10-11 20:12:12 +0300

    sdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Button from './Components/Button/Button';
 
 function App() {
   return (
     <div>
       <h1>Hi i am shopoBot</h1>
+      <Button></Button>
     </div>
   );
 }
